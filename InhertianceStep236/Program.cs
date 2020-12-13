b1384e8e5765efce4a6ccb4b3f27ef3a70059922
student <student@learncodinganywhere.com>
2020-12-12 22:15:08 -0800

    Add project files.

---
+++ InhertianceStep236/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InhertianceStep236
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee badge = new Employee();
+
+            badge.FirstName = "Sample";
+            badge.LastName = "student";
+
+
+            badge.SayName();
+            Console.ReadLine();
+        }
+    }
+    
+}
